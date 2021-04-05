dff89927316b56d42dc6e9a1c6413344a468b40f
Sara <sara.m.adamski@gmail.com>
2021-04-05 18:30:03 -0500

    refactor code to organize funcitionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,23 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to excecute adding li's
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     // prevents browser from automatically refreshing when submitted
     event.preventDefault();
     // adds user data to new li
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+// holds the code that creates a new task HTML element
+var createTaskEl = function(taskDataObj) {
     // creates variable to create a list item object
     var listItemEl = document.createElement("li");
     // identifies the class name of new li to apply CSS styling
@@ -19,11 +30,11 @@ var createTaskHandler = function(event) {
     // give it a class name
     taskInfoEl.className = "task-info";
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     // adds newly created li to bottom of list
     tasksToDoEl.appendChild(listItemEl);
-};
+}
 
 // event listener triggers when user submits (clicks button or types enter) new li
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
